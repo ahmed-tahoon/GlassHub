91213d64917570a2ab08d777055d4f64e7acdbb4
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2023-12-27 01:40:23 +0200

    Create locationService.test.js

---
+++ BakcEnd/tests/locationService.test.js
@@ -0,0 +1,22 @@
+// const LocationService = require("../src/services/locationService");
+// const LocationController = require("../src/controllers/locationController");
+// const locationController = new LocationController(LocationService);
+
+// describe("Add Location", () => {
+//   test("should add a location without logging", async () => {
+//     const title = "Test Location";
+//     const filePath = "/test/path";
+//     const latitude = 40.7128;
+//     const longitude = -74.006;
+
+//     const newLocation = await locationController.createLocation(
+//       title,
+//       filePath,
+//       latitude,
+//       longitude
+//     );
+
+//     expect(newLocation).toBeDefined();
+//     expect(newLocation.title).toBe(title);
+//   });
+// });
